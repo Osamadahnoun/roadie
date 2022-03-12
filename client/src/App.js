06ea28bb9936b4d5db0dece5e9ca6d1ba239a7ef
Osama Dahnoun <omdahnou@ncsu.edu>
2022-03-11 21:46:15 -0500

    imported bootstrap/cleaned up file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import LandingPage from "./pages/LandingPage";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import Authorization from "./pages/Authorization/Authorization";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
+      {/* <Authorization /> */}
     </div>
   );
 }
---
+++ client/src/pages/AllPosts/AllPosts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Allposts = () => {
+  return <div>Allposts</div>;
+};
+
+export default Allposts;
