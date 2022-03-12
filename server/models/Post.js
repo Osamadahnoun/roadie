51b2fe5050de28d5ff033e063b60da053ee4d99e
Claire <cewright1998@gmail.com>
2022-03-12 15:26:58 -0500

    created server models for User, Post, and Comment

---
+++ server/models/Post.js
@@ -0,0 +1,62 @@
+const { Schema, model } = require('mongoose');
+const commentSchema = require('./Comment');
+const dateFormat = require('../utils/dateFormat');
+
+const postSchema = new Schema(
+  {
+    postText: {
+      type: String,
+      required: 'You need to create a post!',
+      minlength: 1,
+      maxlength: 100
+    },
+    location: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 50
+    },
+    // image of location?
+    cost : {
+      // find a way to have user select number of dollar signs instead of inputting a number?
+      // or have their post show a number of dollar signs based on the number they input
+      type: Number,
+      maxlength: 10
+    },
+    heritages: {
+      type: String,
+      maxlength: 50
+    },
+    placesToVisit: {
+      type: String,
+      maxlength: 50
+    },
+    accessibility: {
+      type: String,
+      maxlength: 100
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    comments: [commentSchema]
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+postSchema.virtual('commentCount').get(function() {
+  return this.comment.length;
+});
+
+const Post = model('Post', postSchema);
+
+module.exports = Post;
