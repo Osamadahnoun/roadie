9263874ce6119eb22c2694f75cb32577185f41e7
Claire <cewright1998@gmail.com>
2022-03-14 17:26:27 -0400

    created typeDefs for server

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,58 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    posts: [Posts]
+    friends: [User]
+  }
+
+  type Post {
+    _id: ID
+    postText: String
+    location: String
+    cost: Int
+    heritages: Sring
+    placesToVisit: String
+    accessibility: String
+    other: String
+    createdAt: String
+    username: String
+    commentCount: Int
+    comments: [Comment]
+  }
+
+  type Comment {
+    _id: ID
+    commentBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    posts(username: String): [Post]
+    post(_id: ID!): Post
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addPost(postText: String!, location: String!, cost: Int, heritages: String, placesToVisit: String, accessibility: String, other: String): Post
+    addComment(postId: ID!, commentBody: String!): Post
+    addFriend(friendId: ID!): User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
+module.exports = typeDefs;
