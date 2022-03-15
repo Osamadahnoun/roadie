53ff885e9d23a6d17f35119760bd55083dce4feb
Austin Gilmore <austingilmora@gmail.com>
2022-03-15 18:50:08 -0400

    added queries to server

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,7 +14,25 @@ const resolvers = {
 
             throw new AuthenticationError('Not logged in');
         },
-
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('friends')
+                .populate('posts')
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+            .select('-__v -password')
+            .populate('friends')
+            .populate('posts')
+        },
+        posts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Post.find(params).sort({ createdAt: -1})
+        },
+        post: async (parent, { _id }) => {
+            return Post.findOne({ _id })
+        }
     },
 
     Mutation: {
