e676c5ccc049e1c75de27f5d9f995921b8332178
Osama Dahnoun <omdahnou@ncsu.edu>
2022-03-15 21:18:57 -0400

    singlePostPage and comment area complete

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,9 @@ import Authorization from "./pages/Authorization/Authorization";
 import Allposts from "./pages/AllPosts/AllPosts";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Profile from "./pages/Profile/Profile";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SinglePost from "./pages/SinglePost/SinglePost";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -14,7 +16,9 @@ function App() {
         <Header />
         <Switch>
           <Route exact path="/" component={Allposts} />
+          <Route exact path="/post" component={SinglePost} />
           <Route exact path="/auth" component={Authorization} />
+          <Route exact path="/profile" component={Profile} />
           <Route component={Allposts} />
         </Switch>
         <Footer />
--- client/src/pages/AllPosts/AllPosts.js
+++ client/src/pages/AllPosts/AllPosts.js
@@ -4,7 +4,7 @@ import Posts from "../../components/Posts/Posts";
 const Allposts = () => {
   return (
     <div className="container d-flex flex-column justify-content-center">
-      <h2 className="m-5">Travel Logs</h2>
+      <h2 className="m-5 home-title">Travel Logs</h2>
       <Posts />
     </div>
   );
