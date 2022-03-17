505beb03b85cd4c56fbe74a20e029ddc8d1e4f21
Osama Dahnoun <omdahnou@ncsu.edu>
2022-03-16 20:27:29 -0400

    commit

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import "./Header.css";
-import { Navbar, Nav, Container, NavDropdown, Modal, Tab } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  Container,
+  NavDropdown,
+  Modal,
+  Tab,
+} from "react-bootstrap";
 import AddPostModal from "../AddPostModal/AddPostModal";
 import { Link } from "react-router-dom";
-import Auth from '../../utils/auth'
-import Signup from '../Authorization/Signup/Signup'
-import Login from '../Authorization/Login/Login'
+import Auth from "../../utils/auth";
+import Signup from "../Authorization/Signup/Signup";
+import Login from "../Authorization/Login/Login";
 
 const Header = () => {
   const [showModal, setShowModal] = useState(false);
@@ -25,15 +32,11 @@ const Header = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link className="text1">
-                  <Link to="/allposts">
-                    <p>Home</p>
-                  </Link>
+                <Nav.Link className="text1" as={Link} to="/allposts">
+                  <p>Home</p>
                 </Nav.Link>
-                <Nav.Link className="text1">
-                  <Link to="/profile">
-                    <p>My Profile</p>
-                  </Link>
+                <Nav.Link className="text1" as={Link} to="/profile">
+                  <p>My Profile</p>
                 </Nav.Link>
                 <NavDropdown
                   title="Action"
@@ -46,45 +49,48 @@ const Header = () => {
                     </AddPostModal>
                   </NavDropdown.Item>
                 </NavDropdown>
-               {/* if user is logged in show saved books and logout */}
-              {Auth.loggedIn() ? (
-                <>
-                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
-                </>
-              ) : (
-                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
-              )}
-            </Nav>
-          </Navbar.Collapse>
+                {/* if user is logged in show saved books and logout */}
+                {Auth.loggedIn() ? (
+                  <>
+                    <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+                  </>
+                ) : (
+                  <Nav.Link onClick={() => setShowModal(true)}>
+                    Login/Sign Up
+                  </Nav.Link>
+                )}
+              </Nav>
+            </Navbar.Collapse>
           </div>
         </Container>
       </Navbar>
       {/* set modal data up */}
       <Modal
-        size='lg'
+        size="lg"
         show={showModal}
         onHide={() => setShowModal(false)}
-        aria-labelledby='signup-modal'>
+        aria-labelledby="signup-modal"
+      >
         {/* tab container to do either signup or login component */}
-        <Tab.Container defaultActiveKey='login'>
+        <Tab.Container defaultActiveKey="login">
           <Modal.Header closeButton>
-            <Modal.Title id='signup-modal'>
-              <Nav variant='pills'>
+            <Modal.Title id="signup-modal">
+              <Nav variant="pills">
                 <Nav.Item>
-                  <Nav.Link eventKey='login'>Login</Nav.Link>
+                  <Nav.Link eventKey="login">Login</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
+                  <Nav.Link eventKey="signup">Sign Up</Nav.Link>
                 </Nav.Item>
               </Nav>
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <Tab.Content>
-              <Tab.Pane eventKey='login'>
+              <Tab.Pane eventKey="login">
                 <Login handleModalClose={() => setShowModal(false)} />
               </Tab.Pane>
-              <Tab.Pane eventKey='signup'>
+              <Tab.Pane eventKey="signup">
                 <Signup handleModalClose={() => setShowModal(false)} />
               </Tab.Pane>
             </Tab.Content>
