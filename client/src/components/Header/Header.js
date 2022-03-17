411f90c9bd5141112cdd18205e43e7e75cf2ef2b
Erinn Schneider <erinnaschneider@gmail.com>
2022-03-16 20:16:29 -0400

    login/signup modal

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
-import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, Container, NavDropdown, Modal, Tab } from "react-bootstrap";
 import AddPostModal from "../AddPostModal/AddPostModal";
 import { Link } from "react-router-dom";
+import Auth from '../../utils/auth'
+import Signup from '../Authorization/Signup/Signup'
+import Login from '../Authorization/Login/Login'
 
 const Header = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="w-100">
       <Navbar bg="light" expand="lg" className="p-3">
@@ -41,22 +46,51 @@ const Header = () => {
                     </AddPostModal>
                   </NavDropdown.Item>
                 </NavDropdown>
-                <Nav.Link className="text1">
-                  <Link to="/login">
-                    <p>Login</p>
-                  </Link>
-                </Nav.Link>
-                <Nav.Link className="text1">
-                  <Link to="/signup">
-                    <p>Signup</p>
-                  </Link>
-                </Nav.Link>
-
-              </Nav>
-            </Navbar.Collapse>
+               {/* if user is logged in show saved books and logout */}
+              {Auth.loggedIn() ? (
+                <>
+                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+                </>
+              ) : (
+                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
           </div>
         </Container>
       </Navbar>
+      {/* set modal data up */}
+      <Modal
+        size='lg'
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        aria-labelledby='signup-modal'>
+        {/* tab container to do either signup or login component */}
+        <Tab.Container defaultActiveKey='login'>
+          <Modal.Header closeButton>
+            <Modal.Title id='signup-modal'>
+              <Nav variant='pills'>
+                <Nav.Item>
+                  <Nav.Link eventKey='login'>Login</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Tab.Content>
+              <Tab.Pane eventKey='login'>
+                <Login handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+              <Tab.Pane eventKey='signup'>
+                <Signup handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+            </Tab.Content>
+          </Modal.Body>
+        </Tab.Container>
+      </Modal>
     </div>
   );
 };
