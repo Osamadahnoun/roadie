4883cf175ca247a40cf460990cae5f946c5fc30f
Erinn Schneider <erinnaschneider@gmail.com>
2022-03-20 09:40:41 -0400

    pausing point for color changes

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -37,7 +37,7 @@ const Header = () => {
           <div>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
+              <Nav className="me-auto hoverPointer">
                 <Nav.Link className="text1" as={Link} to="/allposts">
                   Home
                 </Nav.Link>
@@ -51,17 +51,17 @@ const Header = () => {
                 {/* if user is logged in show saved books and logout */}
                 {Auth.loggedIn() ? (
                   <>
-                    <Nav.Link className="text1" as={Link} to="/profile">
+                    <Nav.Link className="text1 hoverPointer" as={Link} to="/profile">
                       My Profile
                     </Nav.Link>
                     <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
                   </>
                 ) : (
-                  <Nav.Link onClick={() => setShowModal(true)}>
+                  <Nav.Link className="hoverPointer" onClick={() => setShowModal(true)}>
                     <p>Login/Sign Up</p>
                   </Nav.Link>
                 )}
-                <Nav.Link onClick={getCheckout}>Donate</Nav.Link>
+                <Nav.Link className="hoverPointer" onClick={getCheckout}>Donate</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </div>
@@ -76,14 +76,14 @@ const Header = () => {
       >
         {/* tab container to do either signup or login component */}
         <Tab.Container defaultActiveKey="login">
-          <Modal.Header closeButton>
+          <Modal.Header closeButton className="modalHeader">
             <Modal.Title id="signup-modal">
               <Nav variant="pills">
                 <Nav.Item>
-                  <Nav.Link eventKey="login">Login</Nav.Link>
+                  <Nav.Link className="hoverPointer login" eventKey="login">Login</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey="signup">Sign Up</Nav.Link>
+                  <Nav.Link className="hoverPointer signup" eventKey="signup">Sign Up</Nav.Link>
                 </Nav.Item>
               </Nav>
             </Modal.Title>
