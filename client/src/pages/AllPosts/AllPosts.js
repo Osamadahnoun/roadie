0607dd213be5f5f674ca846763b4525fe838062f
Erinn Schneider <erinnaschneider@gmail.com>
2022-03-22 20:16:00 -0400

    icon twiddling

--- client/src/pages/AllPosts/AllPosts.js
+++ client/src/pages/AllPosts/AllPosts.js
@@ -4,6 +4,7 @@ import { useQuery } from "@apollo/client";
 import { GET_ALL_POSTS } from "../../utils/queries";
 import AddPostModal from "../../components/AddPostModal/AddPostModal";
 import './AllPosts.css';
+import { FaPlus } from 'react-icons/fa';
 
 const Allposts = () => {
   const { loading, data } = useQuery(GET_ALL_POSTS);
@@ -13,7 +14,7 @@ const Allposts = () => {
     <div className="container d-flex flex-column justify-content-center">
       <h2 className="m-5 home-title">Travel Logs</h2>
       <AddPostModal>
-        <button className="btn d-block w-100">Add a Post</button>
+        <button className="btn d-block w-100 add-a-post-btn">Add a Post</button>
       </AddPostModal>
       {loading ? <div>Loading...</div> : <Posts posts={posts} />}
     </div>
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -7,6 +7,7 @@ import Auth from "../../utils/auth";
 import FriendsListModal from "../../components/FriendsListModal/FriendsListModal";
 import { Button } from "@chakra-ui/react";
 import { ADD_FRIEND } from "../../utils/mutations";
+import { FaPlus } from 'react-icons/fa';
 
 const Profile = () => {
   const { username: userParam } = useParams();
@@ -18,7 +19,7 @@ const Profile = () => {
   const user = data?.me || data?.user || {};
 
   const [friendButton, setFriendButton] = useState(
-    `Add ${userParam} to friends list`
+    `${<FaPlus />}Add ${userParam} to friends list`
   );
 
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
